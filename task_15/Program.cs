2af0f0130a9c194498b1264dc849a832c285a406
Griffon2012 <trifonov-ga@yandex.ru>
2022-10-18 00:37:37 +0300

    add task_15

---
+++ task_15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите номер дня недели: ");
+int weekDayNumber = Convert.ToInt32(Console.ReadLine());
+
+if ( weekDayNumber < 1 || weekDayNumber > 7)
+{
+    Console.WriteLine("Число не соответствует дню недели");
+}
+else
+{
+    if (weekDayNumber == 6 || weekDayNumber == 7)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
