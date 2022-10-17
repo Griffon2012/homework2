9f7a67f137a619aab2bf1521fd73dc31cdfde8ce
Griffon2012 <trifonov-ga@yandex.ru>
2022-10-18 00:29:36 +0300

    add task_13 edit gitignore (add ignore debug vscode file)

---
+++ task_13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number / 100 == 0)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    int tempNumber = number,
+        lastNumber = tempNumber % 10;
+    while (tempNumber / 1000 > 0)
+    {
+        tempNumber = tempNumber / 10;
+        lastNumber = tempNumber % 10;
+    }
+    Console.WriteLine(lastNumber);
+}
+
